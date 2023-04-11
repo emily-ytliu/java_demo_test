867899bc90faa00550c2cc9182b78fc852f15800
yiting <yiting50425@gmail.com>
2023-04-11 11:38:20 +0800

    add Bank entity

---
+++ src/main/java/com/example/java_demo_test/entity/Bank.java
@@ -0,0 +1,21 @@
+package com.example.java_demo_test.entity;
+
+public class Bank {
+	int balance;
+	int account;
+	
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public int getAccount() {
+		return account;
+	}
+	public void setAccount(int account) {
+		this.account = account;
+	}
+	
+	
+}
